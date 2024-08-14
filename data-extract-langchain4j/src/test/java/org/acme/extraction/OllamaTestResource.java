c23b388c0835e531b98998007b3fa46f988bef25
aldettinger <aldettinger@gmail.com>
2024-08-14 15:42:32 +0200

    Improved OllamaTestResource management of tags and modes

--- data-extract-langchain4j/src/test/java/org/acme/extraction/OllamaTestResource.java
+++ data-extract-langchain4j/src/test/java/org/acme/extraction/OllamaTestResource.java
@@ -1,5 +1,6 @@
 package org.acme.extraction;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,7 +17,8 @@ import static java.lang.String.format;
 public class OllamaTestResource implements QuarkusTestResourceLifecycleManager {
 
     private static final Logger LOG = LoggerFactory.getLogger(OllamaTestResource.class);
-    // TODO: don't use latest version, switch to explicit version
+
+    // langchain4j offers only latest tag for ollama-codellama, hence we do use latest until more tags are introduced
     private static final String OLLAMA_IMAGE = "langchain4j/ollama-codellama:latest";
     private static final int OLLAMA_SERVER_PORT = 11434;
 
@@ -27,15 +29,17 @@ public class OllamaTestResource implements QuarkusTestResourceLifecycleManager {
 
     private static final String MODE_MOCK = "mock";
     private static final String MODE_RECORDING = "record";
+    private static final String MODE_CONTAINER = "container";
 
     /**
      * The testMode value could be defined, for instance by invoking: mvn clean test -DtestMode=mock. With the default
      * value "mock", the llm is faked based on the last recorded run. With the value "record", tests are run against a
-     * containerized llm while the HTTP interactions with the llm are recorded. Any other value would run the test
-     * against a containzeried llm without recording.
+     * containerized llm while the HTTP interactions with the llm are recorded. With the value "container" tests are run
+     * against a containerized llm without recording. With any other value, an exception is thrown.
      */
-    private boolean mock;
-    private boolean recording;
+    private boolean isMockMode;
+    private boolean isRecordingMode;
+    private boolean isContainerMode;
 
     private static final String BASE_URL_FORMAT = "http://%s:%s";
 
@@ -44,12 +48,17 @@ public class OllamaTestResource implements QuarkusTestResourceLifecycleManager {
 
         // Check the test running mode
         String testMode = System.getProperty("testMode", MODE_MOCK);
-        mock = MODE_MOCK.equals(testMode);
-        recording = MODE_RECORDING.equals(testMode);
+        isMockMode = MODE_MOCK.equals(testMode);
+        isRecordingMode = MODE_RECORDING.equals(testMode);
+        isContainerMode = MODE_CONTAINER.equals(testMode);
+        if (!isMockMode && !isRecordingMode && !isContainerMode) {
+            throw new IllegalArgumentException(
+                    "testMode value should be one of " + Arrays.asList(MODE_MOCK, MODE_RECORDING, MODE_CONTAINER));
+        }
 
         Map<String, String> properties = new HashMap<>();
 
-        if (mock) {
+        if (isMockMode) {
             LOG.info("Starting a fake Ollama server backed by wiremock");
             initWireMockServer();
         } else {
@@ -62,7 +71,7 @@ public class OllamaTestResource implements QuarkusTestResourceLifecycleManager {
 
             baseUrl = format(BASE_URL_FORMAT, ollamaContainer.getHost(), ollamaContainer.getMappedPort(OLLAMA_SERVER_PORT));
 
-            if (recording) {
+            if (isRecordingMode) {
                 LOG.info("Recording interactions with the Ollama server backed by testcontainers");
                 initWireMockServer();
             }
@@ -76,7 +85,7 @@ public class OllamaTestResource implements QuarkusTestResourceLifecycleManager {
     private void initWireMockServer() {
         wireMockServer = new WireMockServer();
         wireMockServer.start();
-        if (recording) {
+        if (isRecordingMode) {
             wireMockServer.resetMappings();
             wireMockServer.startRecording(baseUrl);
         }
@@ -93,9 +102,9 @@ public class OllamaTestResource implements QuarkusTestResourceLifecycleManager {
             LOG.error("An issue occurred while stopping " + ollamaContainer.getNetworkAliases(), ex);
         }
 
-        if (mock) {
+        if (isMockMode) {
             wireMockServer.stop();
-        } else if (recording) {
+        } else if (isRecordingMode) {
             wireMockServer.stopRecording();
             wireMockServer.saveMappings();
         }
